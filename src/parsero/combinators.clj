0399b85031aff333b7111a66b48d765db3312a22
Alejandro Gómez <alejandro@dialelo.com>
2013-12-15 21:56:48 +0100

    Extract combinators to a file and make `many` non-deterministic

---
+++ src/parsero/combinators.clj
@@ -0,0 +1,85 @@
+(ns parsero.combinators
+  (:require [clojure.algo.monads :refer [domonad]])
+  (:require [parsero.core :refer [parser-m m-plus-parser m-result-parser m-result-parser]])
+  (:import [java.lang Character]))
+
+
+(defn any-char
+  "Accepts a single character."
+  [s]
+  (if (empty? s)
+    nil
+    (list (first s) (.substring s 1))))
+
+(defn char-satisfies
+  [p]
+  (domonad parser-m
+    [x any-char
+     :when (p x)]
+    x))
+
+(def digit (char-satisfies #(Character/isDigit %)))
+(def lower (char-satisfies #(Character/isLowerCase %)))
+(def upper (char-satisfies #(Character/isUpperCase %)))
+(def letter (m-plus-parser lower upper))
+(def alphanumeric (m-plus-parser letter digit))
+
+(defn many
+  "Given a parser, return another that
+  tries to apply it to the input string
+  as many times as possible."
+  [p]
+  (m-plus-parser
+    (domonad parser-m
+      [x p
+       xs (many p)]
+      (cons x xs))
+    (m-result-parser [])))
+
+(defn many1
+  "Like `many` but it fails if the given
+  parser doesn't succeed at least once."
+  [p]
+  (domonad parser-m
+    [x p
+     xs (many p)]
+    (cons x xs)))
+
+(def word (many1 letter))
+(def number (many1 digit))
+
+(defn string
+  "Create a parser that parses the given string."
+  [s]
+  (if (empty? s)
+    (m-result-parser "")
+    (let [char-p (char-satisfies #(= (first s) %))]
+      (domonad parser-m
+        [x char-p
+         xs (string (.substring s 1))]
+        (str x xs)))))
+
+(defn sep-by1
+  [p sep-p]
+  (domonad parser-m
+    [x p
+     xs (many (domonad parser-m
+                [_ sep-p
+                 y p]
+                 y))]
+    (cons x xs)))
+
+(defn surrounded-by
+  [prefix-p p suffix-p]
+  (domonad parser-m
+    [_ prefix-p
+     x p
+     _ suffix-p]
+     x))
+
+;(def int-list
+  ;(surrounded-by (char-satisfies #(= % \[))
+                 ;(sep-by1 number (char-satisfies #(= % \,)))
+                 ;(char-satisfies #(= % \]))))
+
+;(int-list "[1,2,3,4]")
--- src/parsero/core.clj
+++ src/parsero/core.clj
@@ -1,14 +1,12 @@
 (ns parsero.core
-  (:require [clojure.algo.monads :refer [defmonad domonad m-plus]])
-  (:import [java.lang Character]))
+  (:require [clojure.algo.monads :refer [defmonad]]))
 
-; The parser monad
 
-(defn- m-result-parser
+(defn m-result-parser
   [v]
   (fn [s] (list v s)))
 
-(defn- m-bind-parser
+(defn m-bind-parser
   [p f]
   (fn [s]
     (let [result (p s)]
@@ -16,11 +14,11 @@
         (let [np (f (first result))]
           (np (second result)))))))
 
-(defn- m-zero-parser
+(defn m-zero-parser
   [s]
   nil)
 
-(defn- m-plus-parser
+(defn m-plus-parser
   [& ps]
   (fn [s]
     (first
@@ -32,61 +30,3 @@
    m-bind   m-bind-parser
    m-zero   m-zero-parser
    m-plus   m-plus-parser])
-
-; 
-
-(defn any-char
-  "Accepts a single character."
-  [s]
-  (if (empty? s)
-    nil
-    (list (first s) (.substring s 1))))
-
-(defn char-satisfies
-  [p]
-  (domonad parser-m
-    [x any-char
-     :when (p x)]
-    x))
-
-(def digit (char-satisfies #(Character/isDigit %)))
-(def lower (char-satisfies #(Character/isLowerCase %)))
-(def upper (char-satisfies #(Character/isUpperCase %)))
-
-(def letter (m-plus-parser lower upper))
-(def alphanumeric (m-plus-parser letter digit))
-
-(defn many
-  "Given a parser, return another that
-  tries to apply it to the input string
-  as many times as possible."
-  [p]
-  (m-plus-parser
-    (domonad parser-m
-      [x p
-       xs (many p)]
-      (str x xs))
-    (m-result-parser "")))
-
-(defn many1
-  "Like `many` but it fails if the given
-  parser doesn't succeed at least once."
-  [p]
-  (domonad parser-m
-    [x p
-     xs (many p)]
-    (str x xs)))
-
-(def word (many1 letter))
-(def number (many1 digit))
-
-(defn string
-  "Create a parser that parses the given string."
-  [s]
-  (if (empty? s)
-    (m-result-parser "")
-    (let [char-p (char-satisfies #(= (first s) %))]
-      (domonad parser-m
-        [x char-p
-         xs (string (.substring s 1))]
-        (str x xs)))))
