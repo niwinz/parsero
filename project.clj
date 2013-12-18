5d8c2bd225a312d93a27058719185c721e8c0b16
Andrey Antukh <niwi@niwi.be>
2013-12-18 20:43:03 +0100

    Improvements on project.clj

--- project.clj
+++ project.clj
@@ -2,6 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "MIT License"}
+  :profiles {:uberjar {:aot :all}
+             :dev {:main bench
+                   :dependencies [[criterium "0.4.2"]
+                                  [org.clojure/data.json "0.2.3"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.monads "0.1.4"]
                  [org.clojure/core.typed "0.2.19"]])
